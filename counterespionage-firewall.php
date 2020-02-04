74215f2b5af27ee103cf28fe8db20f69d1cdfc0a
GS McNamara <floodspark@gmail.com>
2020-02-03 23:13:07 -0500

    created template for first php page. Doesn't actually work.

---
+++ counterespionage-firewall.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package Counterespionage_Firewall
+ * @version 1.0
+ */
+/*
+Plugin Name: Counterespionage Firewall
+Plugin URI: http://wordpress.org/extend/plugins/counterespionage-firewall
+Description: The Floodspark Counterespionage Firewall (CEF) WordPress plugin helps you block reconnaissance or otherwise illegitimate traffic including bots and scrapers. CEF for WordPress has some of the features of CEF Pro edition.
+Author: Floodspark
+Version: 1.0
+Author URI: http://floodspark.com
+*/
+
+//user agent string validation method:
+function check_ua() {
+
+}
+
+//perform all validations within:
+function validate() {
+}
+
+//add_action( '', 'validate' );
+
+?>
