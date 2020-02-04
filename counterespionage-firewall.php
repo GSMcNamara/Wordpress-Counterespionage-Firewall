b83939c933699db1c12b1d68b45c8df93496b561
GS McNamara <floodspark@gmail.com>
2020-02-04 00:25:30 -0500

    update description

--- counterespionage-firewall.php
+++ counterespionage-firewall.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: Counterespionage Firewall
 Plugin URI: http://wordpress.org/extend/plugins/counterespionage-firewall
-Description: The Floodspark Counterespionage Firewall (CEF) WordPress plugin helps you block reconnaissance or otherwise illegitimate traffic including bots and scrapers. CEF for WordPress has some of the features of CEF Pro edition.
+Description: CEF is like a web application firewall (WAF) but protects against reconnaissance by hackers and otherwise illegitimate traffic such as bots and scrapers. Increase performance, reduce fraud, thwart attacks, and serve your real customers.
 Author: Floodspark
 Version: 1.0
 Author URI: http://floodspark.com
@@ -14,13 +14,16 @@ Author URI: http://floodspark.com
 
 //user agent string validation method:
 function check_ua() {
-
+	$uas = WP_REST_Request::get_header("User-Agent");
+	echo $uas;
+	echo "mcnamara";
 }
 
 //perform all validations within:
 function validate() {
+	check_ua();
 }
 
-//add_action( '', 'validate' );
+add_action( 'init', 'validate' );
 
 ?>
